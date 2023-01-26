ef5acade382c3f66b04b07c16e84a67bc0547dbc
Bee <79103716+beesmalley@users.noreply.github.com>
2023-01-26 10:06:01 -0500

    hm

--- V1/scripts/bakers.js
+++ V1/scripts/bakers.js
@@ -1,12 +1,21 @@
+var canvasWidth=800;
+var canvasHeight=500;
+
+var kitchen=document.getElementById("canvas");
 
 function startGame() {
     gameCanvas.start();
+
+    player= new createKitchen(30,30,10)
+
 }
 
 var gameCanvas = {
-canvas: document.createElement("canvas"),
-start: function() {
-this.context = this.canvas.getContext("2d"); 
-document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-}
+    canvas: document.createElement("canvas"),
+    start: function() {
+    this.canvas.width=canvasWidth;
+    this.canvas.height=canvasHeight;
+    this.context = this.canvas.getContext("2d"); 
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
 }
