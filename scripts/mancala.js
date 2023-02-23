7894a9375d1ca8f28d54558af31a2996fa2d25f2
Bee <79103716+beesmalley@users.noreply.github.com>
2023-02-23 12:24:47 -0500

    Update mancala.js

---
+++ scripts/mancala.js
@@ -0,0 +1,14 @@
+//OKAY, make the mancala ai
+//lets do it now
+
+//yay
+
+//OK
+
+//what do we need:
+// board setup: two players, each player has six pots and one 
+// score pot.
+
+//basic rules:
+
+
