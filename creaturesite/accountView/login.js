49060f53eea618a41fe7badd3a3ad396cf08408c
Bee <79103716+beesmalley@users.noreply.github.com>
2023-03-02 09:42:52 -0500

    updates

--- creaturesite/accountView/login.js
+++ creaturesite/accountView/login.js
@@ -1,3 +1,9 @@
-$('#button').click(function() {
-    $('#loginform').addClass('myClass');
-  });
+let panel=document.getElementsById('loginform');
+
+panel.addEventListener("click", expand());
+
+function expand(){
+  panel.style.width="300";
+  panel.style.height="300";
+  panel.style.fontSize="28pt";
+}
