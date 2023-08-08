9473a255a0efa61f49119764475839b35245c402
Bee <79103716+beesmalley@users.noreply.github.com>
2023-08-08 13:47:33 -0400

    mancala updates
    building out mancala's base functionality before adding game logic and before brushing up aesthetics

--- scripts/mancala.js
+++ scripts/mancala.js
@@ -1,74 +1,98 @@
-//OKAY, make the mancala ai
-//lets do it now
+document.addEventListener('DOMContentLoaded', () => {
+    const startbutton = document.getElementById("start");
+    let scoreboard = document.getElementById("scoreboard");
+    let score = 0;
+    scoreboard.innerHTML = "Score: "+score;
 
-//yay
+    const pots = [
+        document.getElementById("1"),
+        document.getElementById("2"),
+        document.getElementById("3"),
+        document.getElementById("4"),
+        document.getElementById("5"),
+        document.getElementById("6"),
+        document.getElementById("upit"),
+        document.getElementById("7"),
+        document.getElementById("8"),
+        document.getElementById("9"),
+        document.getElementById("10"),
+        document.getElementById("11"),
+        document.getElementById("12"),
+        document.getElementById("opppit")
+      ]; //we defined the array here so that the stones go in the order we want them to
+    let userScorePit = document.getElementById('upit'); // User's score pit
+    let oppScorePit = document.getElementById('opppit'); //Opponent's Score pit
+    var playing = false;
 
-//OK
+    startbutton.addEventListener('click', () => {
+        pots.forEach(pot => {
+          const circleContainers = pot.querySelectorAll('.circle-container');
+          circleContainers.forEach(container => {
+            container.remove();
+          });
+          if(pot!=userScorePit&&pot!=oppScorePit){
+            const circleContainer = document.createElement('div');
+            circleContainer.classList.add('circle-container');
+            
+            for (let i = 0; i < 3; i++) {
+              const circle = document.createElement('div');
+              circle.classList.add('circle');
+              circleContainer.appendChild(circle);
+            }
+            
+            pot.appendChild(circleContainer);
+          }else{
+            const circleContainer = document.createElement('div');
+            circleContainer.classList.add('circle-container');
+            pot.appendChild(circleContainer);
+          }
+        });
+      
+        playing = true;
+    });      
 
-//TERMS:
-//pot: holds stones. Starts at 3 stones.
-//pit: score pots, one for each player
-//stone: the marbles that are put in the pits or pots
+    pots.forEach(pot => {
+        pot.addEventListener('click', () => {
+          if (playing) {
+            distributeStones(pot); // Call the distributeStones function when a pot is clicked
+          }
+        });
+      });
+    
+    function distributeStones(clickedPot) {
+    const circleContainers = clickedPot.querySelectorAll('.circle-container');
+    const totalStones = calculateStones(clickedPot); // Calculate the number of stones
 
-//what do we need:
-// board setup: two players, each player has six pots and one 
-// score pot.
-
-//basic rules:
-
-//score pits start at 0 
-//each pot holds 3 stones at start
-
-const board = [
-    [0, 4, 4, 4, 4, 4, 4],  // Player 1's pits
-    [4, 4, 4, 4, 4, 4, 0]   // Player 2's pits
-];
-
-let playerTurn = true;  // Player 1 starts
-
-
-function moveStones(playerIndex, pitIndex) {
-    const stonesToMove = board[playerIndex][pitIndex];
-    board[playerIndex][pitIndex] = 0;
-
-    let currentPlayerIndex = playerIndex;
-    let currentPitIndex = pitIndex;
-
-    while (stonesToMove > 0) {
-        currentPitIndex = (currentPitIndex + 1) % 7;  // Circular pit indexing
-        
-        if (currentPlayerIndex === 1 && currentPitIndex === 0) {
-            continue;  // Skip opponent's Mancala pit
-        }
-
-        board[currentPlayerIndex][currentPitIndex]++;
-        stonesToMove--;
+    if (totalStones === 0) {
+        return; // No stones to distribute
     }
-}
-function captureStones(playerIndex, pitIndex) {
-    const stonesCaptured = board[playerIndex][pitIndex];
-    if (stonesCaptured === 1 && board[playerIndex][pitIndex] === 0) {
-        const oppositePitIndex = 6 - pitIndex;  // Calculate the opposite pit index
-        const capturedStones = board[oppositePlayerIndex][oppositePitIndex];
-        
-        board[oppositePlayerIndex][oppositePitIndex] = 0;
-        board[playerIndex][6] += capturedStones + 1;  // Add captured stones to player's Mancala
+    
+    circleContainers.forEach(container => {
+        const circles = container.querySelectorAll('.circle');
+    
+        circles.forEach((circle, index) => {
+        setTimeout(() => {
+            const targetPotIndex = (Array.from(pots).indexOf(clickedPot) + index + 1) % pots.length;
+            const targetPot = pots[targetPotIndex];
+            const targetContainer = targetPot.querySelector('.circle-container'); // Get the target container
+    
+            if (targetPot === userScorePit) {
+                userScorePit.appendChild(circle); // Append the stone to the user's score pit
+              } else {
+                targetContainer.appendChild(circle); // Append the stone to the target container
+              }
+
+        }, index * 500); // Adjust the delay between stone movements
+        });
+    });
     }
-}
-
-function playGame(pitIndex) {
-    if (playerTurn) {
-        moveStones(0, pitIndex);
-        captureStones(0, pitIndex);
-    } else {
-        moveStones(1, pitIndex);
-        captureStones(1, pitIndex);
+    
+//function returns number of stones in a pot
+    function calculateStones(pot) {
+        const stones = pot.querySelectorAll('.circle');
+        return stones.length;
     }
-
-    // Switch turns
-    playerTurn = !playerTurn;
-}
-
+});
 
 
 
